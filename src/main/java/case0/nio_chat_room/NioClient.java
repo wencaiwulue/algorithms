a6339d1c34b2fc06bd90073460fa5682ce0890f4
wencaiwulue <895703375@qq.com>
2019-05-20 11:18:58 +0800

    client create another thread to listening server response

--- src/main/java/case0/nio_chat_room/NioClient.java
+++ src/main/java/case0/nio_chat_room/NioClient.java
@@ -2,23 +2,45 @@ package case0.nio_chat_room;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class NioClient {
+
     public static void main(String[] args) throws Throwable {
+        new NioClient().start();
+    }
+
+    public void start() throws Throwable {
+        // 1, 创建Selector
+        Selector selector = Selector.open();
+        // 2,创建socketChannel,并创建连接
         SocketChannel channel = SocketChannel.open(new InetSocketAddress("localhost", 8000));
-//        channel.connect();
-//        channel.write(ByteBuffer.wrap("i am client".getBytes()));
-//        Scanner scanner = new Scanner(System.in);
-//        while (scanner.hasNextLine()) {
-//            String str = scanner.nextLine();
-//            if (str.equals("q")) {
-//                break;
-//            }
-        channel.write(Charset.forName("UTF-8").encode("abc"));
-//        }
+        // 3,是否需要设置为异步？？
+        channel.configureBlocking(false);
+        // 4, 将channel注册到Selector
+        channel.register(selector, SelectionKey.OP_READ);
+        // 5，循环selector.select()方法
+        // 6,调用selector的SelectorKeys方法，获取事件
+        // 7,按照不同类型的时间，调用不同的业务逻辑
+        // 8,goto step 3
+        new Thread(new NioClientReadHandler(selector)).start();
+
+
+        channel.write(ByteBuffer.wrap("i am client".getBytes()));
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            if (str.equals("q")) {
+                break;
+            }
+            channel.write(Charset.forName("UTF-8").encode(str));
+        }
+
+
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
         while (channel.read(byteBuffer) > 0) {
             byteBuffer.flip();
@@ -26,4 +48,5 @@ public class NioClient {
         }
 
     }
+
 }
---
+++ src/main/java/case0/nio_chat_room/NioClientReadHandler.java
@@ -0,0 +1,58 @@
+package case0.nio_chat_room;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author fengcaiwen
+ * @since 5/20/2019
+ */
+public class NioClientReadHandler implements Runnable {
+
+    private Selector selector;
+
+    public NioClientReadHandler(Selector selector) {
+        this.selector = selector;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (; ; ) {
+                int select = selector.select();
+                if (select == 0) {
+                    continue;
+                }
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey next = iterator.next();
+                    iterator.remove();
+                    if (next.isReadable()) {
+                        readHandler((SocketChannel) next.channel(), selector);
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readHandler(SocketChannel channel, Selector selector) throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        StringBuilder str = new StringBuilder();
+        while (channel.read(byteBuffer) > 0) {
+            byteBuffer.flip();
+            str.append(Charset.forName("UTF-8").decode(byteBuffer));
+        }
+        System.out.println("read from server: " + str);
+        channel.register(selector, SelectionKey.OP_WRITE);
+    }
+}
