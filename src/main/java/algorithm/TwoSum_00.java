aa632346dcb0b1001e3d0a0956ba6e6173c2075e
wencaiwulue <895703375@qq.com>
2019-05-28 10:19:33 +0800

    test

---
+++ src/main/java/algorithm/TwoSum_00.java
@@ -0,0 +1,39 @@
+package algorithm;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author fengcaiwen
+ * @since 5/22/2019
+ */
+public class TwoSum_00 {
+    public static void main(String[] args) throws IOException {
+        getIndex(new int[]{3, 2, 4}, 6);
+    }
+
+    public static int[] getIndex(int[] nums, int target) throws IOException {
+        Map<Integer, Integer> differenceMap = new HashMap<>();
+        Map<Integer, Integer> indexList = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            indexList.put(nums[i], i);
+            int difference = target - nums[i];
+            if (!differenceMap.containsKey(difference)) {
+                differenceMap.put(difference, i);
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (differenceMap.containsKey(nums[i])) {
+                if (differenceMap.get(nums[i]) == i) {
+                    continue;
+                }
+                System.out.printf("%d, %d", indexList.get(nums[i]), differenceMap.get(nums[i]));
+                return new int[]{indexList.get(nums[i]), differenceMap.get(nums[i])};
+            }
+        }
+        return new int[]{-1, -1};
+//        System.out.println(map.toString());
+    }
+}
