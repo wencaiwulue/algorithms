db393cff7cc7a2cfe8b9be0adcb0b5fc4a425b6b
wencaiwulue <895703375@qq.com>
2019-05-30 18:39:23 +0800

    test

--- src/main/java/algorithm/ElavatorTest.java
+++ src/main/java/algorithm/ElavatorTest.java
@@ -1,9 +1,42 @@
 package algorithm;
 
+import com.google.common.io.BaseEncoding;
+
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * @author fengcaiwen
  * @since 5/29/2019
+ * <p>
+ * 电梯运行算法，
+ * 电梯调度算法
  */
 public class ElavatorTest {
-    public static class Elavator{}
+
+    public static void main(String[] args) {
+
+    }
+
+    public int byElevator(List<Elevator> elevatorList, int target) {
+        return -1;
+    }
+
+    public int getNextTarget(int current, Queue<Integer>[] targets) {
+        return -1;
+    }
+
+    public int getNextTarget(int current, Queue<Integer> target) {
+        return -1;
+    }
+
+
+
+    public static class Elevator {
+        private String name;
+        private volatile int current;
+        private int target;
+        private Queue<Integer>[] targets;
+    }
 }
--- src/main/java/algorithm/ReverseTest.java
+++ src/main/java/algorithm/ReverseTest.java
@@ -5,4 +5,75 @@ package algorithm;
  * @since 5/28/2019
  */
 public class ReverseTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(2);
+        l2.next.next = new ListNode(3);
+        l2.next.next.next = new ListNode(4);
+        ListNode node = reverse(l2);
+        System.out.println(node.value);
+
+    }
+
+    public static ListNode reverse(ListNode current) {
+        ListNode nextNode = null;
+        ListNode previousNode = null;
+        while (current != null) {
+            nextNode = current.next;
+            current.next = previousNode;
+
+            previousNode = current;
+            current = nextNode;
+
+        }
+        return previousNode;
+    }
+
+    public static ListNode dec(ListNode header){
+        while (header!=null){
+
+        }
+        return null;
+    }
+
+
+//    public static ListNode reverse0(ListNode current) {
+//        //initialization
+//        ListNode previousNode = null;
+//        ListNode nextNode = null;
+//
+//        while (current != null) {
+//            //save the next node
+//            nextNode = current.next;
+//            //update the value of "next"
+//            current.next = previousNode;
+//            //shift the pointers
+//            previousNode = current;
+//            current = nextNode;
+//        }
+//        return previousNode;
+//    }
+
+    public static ListNode reverseRec(ListNode current)
+    {
+        if (current == null || current.next == null) return current;
+        ListNode nextNode = current.next;
+        current.next = null;
+        ListNode reverseRest = reverse(nextNode);
+        nextNode.next = current;
+        return reverseRest;
+    }
+
+
+    public static class ListNode {
+        private int value;
+        private ListNode next;
+
+        public ListNode(int value) {
+            this.value = value;
+        }
+    }
+
+
 }
