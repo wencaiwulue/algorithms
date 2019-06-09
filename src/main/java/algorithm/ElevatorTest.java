f30c188b871cc3bffacdaf9f1f777272734f5aa2
wencaiwulue <895703375@qq.com>
2019-06-09 22:40:42 +0800

    test

--- src/main/java/algorithm/ElevatorTest.java
+++ src/main/java/algorithm/ElevatorTest.java
@@ -1,8 +1,6 @@
 package algorithm;
 
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 
 /**
  * @author fengcaiwen
@@ -14,33 +12,62 @@ import java.util.concurrent.BlockingQueue;
 public class ElevatorTest {
 
     public static void main(String[] args) throws InterruptedException {
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new LinkedHashSet<>();
         set.add(3);
+        set.add(2);
         set.add(7);
         set.add(10);
         set.add(5);
         set.add(1);
-        Elevator elevator = new Elevator("a", 0, 3, set);
-        new ElevatorTest().run(elevator);
-
+        Elevator elevator = new Elevator("a", 0, (Integer) set.toArray()[0], set);
+        new Thread(() -> {
+            try {
+                new ElevatorTest().run(elevator);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNextInt()) {
+                int i = scanner.nextInt();
+                elevator.getTargets().add(i);
+                System.out.println("we get num: " + i);
+            }
+        }).start();
     }
 
     public void run(Elevator elevator) throws InterruptedException {
-        int target = getNextMiddleTarget(elevator);
+        int target = elevator.target;
+        int middleTarget = getNextMiddleTarget(elevator);
+        if (target == Integer.MIN_VALUE) {
+            System.out.println("done");
+            return;
+        }
         int direction = getDirection(elevator);
+        boolean isRunning = middleTarget != elevator.current;
 
         //1, running
-        if (direction != 0) {
-            System.out.println("running to floor: " + elevator.current);
+        if (isRunning) {
+            System.out.println("running to floor: " + elevator.current + ", target is: " + target);
             Thread.sleep(500);
             elevator.current += direction == 1 ? 1 : direction == -1 ? -1 : 0;
             run(elevator);
 
         } else {
             // 2,arrived
-            elevator.targets.remove(elevator.target);
-            System.out.println("arrived floor:" + elevator.target);
-            elevator.target = getNextMiddleTarget(elevator);
+            elevator.targets.remove(middleTarget);
+            System.out.println("arrived floor:" + middleTarget);
+            if (middleTarget != elevator.target) {
+
+            } else {
+                Object[] objects = elevator.getTargets().toArray();
+                if (objects.length == 0) {
+                    elevator.target = Integer.MIN_VALUE;
+                } else {
+                    elevator.target = (int) objects[0];
+                }
+            }
             Thread.sleep(1000);
             run(elevator);
         }
@@ -56,21 +83,25 @@ public class ElevatorTest {
         List<Integer> result = new ArrayList<>();
         //1, down
         if (difference > 0) {
-            for (int i = elevator.current; i > elevator.target; i--) {
-                if (elevator.targets.contains(i)) {
-                    result.add(i);
+            boolean block = false;
+            for (Integer next : elevator.getTargets()) {
+                if (next >= elevator.target && next <= elevator.current) {
+                    if (!block) {
+                        result.add(next);
+                    }
+                } else {
+                    block = true;
                 }
             }
             Collections.sort(result);
             return result.get(result.size() - 1);
         } else if (difference < 0) {
             // 2，up
-            for (int i = elevator.current; i < elevator.target; i++) {
-                if (elevator.targets.contains(i)) {
-                    result.add(i);
+            for (Integer next : elevator.getTargets()) {
+                if (next >= elevator.current && next <= elevator.target) {
+                    result.add(next);
                 }
             }
-
             Collections.sort(result);
             return result.get(0);
         } else {
@@ -101,7 +132,7 @@ public class ElevatorTest {
         private String name;
         private int current;
         private int target;
-        private Set<Integer> targets;
+        private volatile Set<Integer> targets;
 
         public Elevator() {
         }
@@ -143,6 +174,22 @@ public class ElevatorTest {
 
         public void setTargets(Set<Integer> targets) {
             this.targets = targets;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Elevator elevator = (Elevator) o;
+            return current == elevator.current &&
+                    target == elevator.target &&
+                    Objects.equals(name, elevator.name) &&
+                    Objects.equals(targets, elevator.targets);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, current, target, targets);
         }
     }
 }
