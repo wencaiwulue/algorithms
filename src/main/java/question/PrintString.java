67c2c74584bcf010f8fc3f62990c67355fac1466
wencaiwulue <895703375@qq.com>
2019-06-14 15:02:44 +0800

    test

--- src/main/java/question/PrintString.java
+++ src/main/java/question/PrintString.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class PrintString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
