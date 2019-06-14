11db6e15b77320cda434a5e968659fcd3ec00291
wencaiwulue <895703375@qq.com>
2019-06-14 11:28:59 +0800

    test

--- src/main/java/operator/XorTest.java
+++ src/main/java/operator/XorTest.java
@@ -4,8 +4,11 @@ package operator;
  * @author fengcaiwen
  * @since 6/14/2019
  */
+@SuppressWarnings("all")
 public class XorTest {
     public static void main(String[] args) {
-        System.out.println(4 ^ 4 ^ 4 ^ 3 ^ 3 ^ 3);
+        System.out.println(7 ^ 7 ^ 7 ^ 5 ^ 5 ^ 3 ^ 7);
+        System.out.println(7 | 5);
+        System.out.println(7 & 5);
     }
 }
