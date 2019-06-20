548d34c64e79b3ad66d4edd538cba670d3fa0d69
wencaiwulue <895703375@qq.com>
2019-06-20 09:11:44 +0800

    test

--- src/main/java/trie/ACTest.java
+++ src/main/java/trie/ACTest.java
@@ -8,16 +8,23 @@ import java.util.Arrays;
  */
 public class ACTest {
     public static void main(String[] args) {
+        String s = "aaaa";
 
-        Arrays.stream(test("chinchilla")).forEach(System.out::println);
+        Arrays.stream(build(s)).forEach(System.out::println);
+        System.out.println("-----------------------------");
+        Arrays.stream(build0(s)).forEach(System.out::println);
+//        pare("chinchilla", "chinchilla");
     }
 
-    private static int[] test(String str) {
+    /**
+     * by myself
+     */
+    private static int[] build(String str) {
 
         int m = str.length();
         char[] chars = str.toCharArray();
 
-        int[] next = new int[m];
+        int[] next = new int[m + 1];
         next[0] = -1;
         int i = 1, j = 0;
 
@@ -33,5 +40,58 @@ public class ACTest {
             }
         }
         return next;
+    }
+
+    /**
+     * copy
+     */
+    private static int[] build0(String str) {
+
+        int m = str.length();
+        char[] chars = str.toCharArray();
+
+        int[] next = new int[m + 1];
+        next[0] = -1;
+        int i = 0, j = -1;
+
+        while (i < m) {
+            if (j < 0 || chars[i] == chars[j]) {
+                i++;
+                j++;
+                next[i] = j;
+            } else {
+                j = next[j];
+            }
+        }
+        return next;
+    }
+
+    public static void pare(String content, String pattern) {
+        int[] next = build(pattern);
+
+        char[] contentChar = content.toCharArray();
+        char[] patternChar = pattern.toCharArray();
+
+        int n = contentChar.length;
+        int m = patternChar.length;
+
+        int i = 1, j = 0;
+        while (i < n - m && j != m) {
+            int a = contentChar[i];
+            int b = patternChar[j];
+            if (a == b) {
+                i++;
+                j++;
+            } else {
+                j = next[j];
+            }
+        }
+//        System.out.println(j);
+//        System.out.println(i);
+//        System.out.println(m);
+        if (j == m) {
+            System.out.println("ok");
+        }
+
     }
 }
