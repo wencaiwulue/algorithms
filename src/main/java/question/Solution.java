3e2660df73dafd0b384a098508eccf5532062340
wencaiwulue <895703375@qq.com>
2019-07-03 17:27:49 +0800

    test

--- src/main/java/question/Solution.java
+++ src/main/java/question/Solution.java
@@ -13,43 +13,35 @@ public class Solution {
     }
 
     /**
+     * 可以跳1~n个台阶
+     *
      * n=1 1 --1种
      * n=2 11 2 --2种
      * n=3 111 12 21 3 --4种
-     * n=4 1111 112 121 211 13 31 4 --7种
-     * n=5 11111 1112 1121 1211 2111 113 131 311 14 41 5 --11种
+     * n=4 1111 112 121 211 22 13 31 4 --8种
+     * n=5 11111 1112 1121 1211 2111 122 221 212 113 131 311 32 23 14 41 5 --16种
      * 归纳：
-     * f(n) = 1+2+3+4+..+n-1 + 1
+     * f(n) = 2^(n-1)
      */
     private static int climbStairs0(int n) {
-        return n * (n - 1) / 2 + 1;
+        return (int) Math.pow(2, n - 1);
     }
 
     /**
+     * 只能跳1或者2个台阶
+     *
      * n=1 1 --1种
      * n=2 11 2 --2种
      * n=3 111 12 21 --3种
-     * n=4 1111 112 121 211 --4种
-     * n=5 11111 1112 1121 1211 2111 --5种
+     * n=4 1111 112 121 211 22 --5种
+     * n=5 11111 1112 1121 1211 2111 122 221 212 --8种
      * 归纳：
-     * f(n) = n
+     * f(n) = f(n-1) + f(n-2)
      */
-    private static int climbStairs1And2(int n) {
-        return n * (n - 1) / 2 + 1;
-    }
-
-    public static int climbStairs(int n) {
-        return climb_Stairs(0, n);
-    }
-
-    public static int climb_Stairs(int i, int n) {
-        if (i > n) {
-            return 0;
-        }
-        if (i == n) {
-            return 1;
-        }
-        return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+    private static int climbStairs(int i) {
+        if (i == 2) return 2;
+        if (i == 1) return 1;
+        return climbStairs(i - 1) + climbStairs(i - 2);
     }
 
 }
