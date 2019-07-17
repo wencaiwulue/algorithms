9f3c21b929da53e78d2ad17ec80a2dfd3338f8df
wencaiwulue <895703375@qq.com>
2019-07-17 15:28:36 +0800

    test

--- src/main/java/computationalGeometry/JudgePointPosition.java
+++ src/main/java/computationalGeometry/JudgePointPosition.java
@@ -5,6 +5,29 @@ package computationalGeometry;
  * @since 7/15/2019
  */
 public class JudgePointPosition {
+
+
+    /**
+     * step:
+     * 1, find the start point
+     * 2, from s to each elements, find the next point t, make t's  right have no point
+     */
+    public void run(Point[] s, int n) {
+
+    }
+
+    /**
+     * find min y from s, if is a list, return min x of list
+     */
+    public Point LTL(Point[] s, int n) {
+        int rank = 0;
+        for (int i = 0; i < s.length; i++)
+            if (s[rank].y > s[i].y || (s[rank].y == s[i].y && s[rank].x > s[i].x))
+                rank = i;
+
+        return s[rank];
+    }
+
     /**
      * ->
      * a：(x1, y1), point A:(x2, y2)
@@ -14,22 +37,12 @@ public class JudgePointPosition {
         return (-y1 * x2 + x1 * y2) > 0;
     }
 
-
+    /**
+     * ->
+     * pq and point s
+     */
     public boolean isLeft(Point p, Point q, Point s) {
         return isLeft(q.x - p.x, q.y - p.y, s.x, s.y);
     }
 
-    public static class Point {
-        public int x;
-        public int y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public static Point of(int x, int y) {
-            return new Point(x, y);
-        }
-    }
 }
---
+++ src/main/java/computationalGeometry/Point.java
@@ -0,0 +1,14 @@
+package computationalGeometry;
+
+/**
+ * @author fengcaiwen
+ * @since 7/17/2019
+ */
+public class Point {
+    public boolean isEdge;
+    public int from;
+    public int to;
+
+    public int x;
+    public int y;
+}
