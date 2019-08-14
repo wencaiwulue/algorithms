c746fc88c773093247da749e494a566c26203e61
wencaiwulue <895703375@qq.com>
2019-08-14 09:59:27 +0800

    test

---
+++ src/main/java/sql/SqlTest.java
@@ -0,0 +1,43 @@
+package sql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @author fengcaiwen
+ * @since 8/14/2019
+ */
+public class SqlTest {
+    public static void main(String[] args) throws SQLException, InterruptedException {
+        String url = "jdbc:mysql://localhost:3306/test";
+        Properties p = new Properties();
+        p.setProperty("user", "root");
+        p.setProperty("password", "12345678");
+        Connection connect = new com.mysql.cj.jdbc.Driver().connect(url, p);
+        // todo: 测试结果表明，无论两种写法都是ok的，可能是因为数据库的行级锁的原因，不过这样有个问题就是数据库的瓶颈，如果要秒杀的商品很多，比如1万个，直接打到库上去可能会炸。
+//        String sql = "update product set quantity = quantity -1 where quantity - 1 >= 0 and id = 1";
+        String sql = "update product set quantity = quantity -1 where quantity >= 1 and id = 1";
+        connect.setAutoCommit(true);
+        connect.createStatement();
+        int i = 0;
+        while (i++ < 10000) {
+            long start = System.currentTimeMillis();
+            new Thread(() -> {
+                while (true)
+                    // 两秒钟够启动2000个左右的线程，机器不同，数量应该不同
+                    while (start + 2000 < System.currentTimeMillis()) {
+                        try {
+                            PreparedStatement ps = connect.prepareStatement(sql);
+                            int i1 = ps.executeUpdate();
+                            System.out.println(i1);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            }).start();
+        }
+        Thread.sleep(1000000);
+    }
+}
