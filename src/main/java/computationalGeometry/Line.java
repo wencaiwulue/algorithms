0f025feb2f9d37467e32be82c4044a98a36e9b1d
wencaiwulue <895703375@qq.com>
2019-08-14 16:42:17 +0800

    test

---
+++ src/main/java/computationalGeometry/Line.java
@@ -0,0 +1,42 @@
+package computationalGeometry;
+
+import java.util.Objects;
+
+/**
+ * @author fengcaiwen
+ * @since 8/14/2019
+ */
+public class Line {
+    public Point p;
+    public Point q;
+
+    public Line() {
+    }
+
+    public Line(Point p, Point q) {
+        this.p = p;
+        this.q = q;
+    }
+
+    public boolean isFrom(Point p) {
+        return this.p.equals(p);
+    }
+
+    public boolean isTo(Point q) {
+        return this.p.equals(q);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(p, line.p) &&
+                Objects.equals(q, line.q);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(p, q);
+    }
+}
--- src/main/java/computationalGeometry/Point.java
+++ src/main/java/computationalGeometry/Point.java
@@ -1,5 +1,7 @@
 package computationalGeometry;
 
+import java.util.Objects;
+
 /**
  * @author fengcaiwen
  * @since 7/17/2019
@@ -31,5 +33,20 @@ public class Point {
                 "x=" + x +
                 ", y=" + y +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return isEdge == point.isEdge &&
+                Double.compare(point.x, x) == 0 &&
+                Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isEdge, x, y);
     }
 }
