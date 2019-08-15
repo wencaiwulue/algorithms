5a7ca5cefc4ed5703ab4582aec1d16182f4eb678
wencaiwulue <895703375@qq.com>
2019-08-15 10:38:45 +0800

    optimize code

--- src/main/java/algorithm/MedianOfTwoSortedArrays.java
+++ src/main/java/algorithm/MedianOfTwoSortedArrays.java
@@ -1,7 +1,5 @@
 package algorithm;
 
-import java.util.Arrays;
-
 /**
  * @author fengcaiwen
  * @since 5/28/2019
@@ -12,20 +10,26 @@ public class MedianOfTwoSortedArrays {
         int n = nums2.length;
         int[] ints = new int[n + m];
         int i = 0, j = 0;
+        int po = 0;
         for (int p = 0; p < (m + n); p++) {
-            if (nums1[i] > nums2[j]) {
-                ints[p++] = nums2[j++];
-            } else {
-                ints[p++] = nums1[i++];
-            }
+            po = p;
+            if (i >= m || j >= n) break;
+            if (nums1[i] > nums2[j])
+                ints[p] = nums2[j++];
+            else
+                ints[p] = nums1[i++];
         }
 
-
+        if (i != m) {
+            System.arraycopy(nums1, i, ints, po, m - i);
+        } else if (j != n) {
+            System.arraycopy(nums2, j, ints, po, n - j);
+        }
 
         double result;
 
         if ((m + n) % 2 == 0) {
-            result = (ints[(m + n) / 2] + ints[(n + m) / 2 + 1]) / 2;
+            result = (ints[(m + n) / 2 - 1] + ints[(n + m) / 2]) / 2d;
         } else {
             result = ints[(m + n) / 2];
         }
@@ -34,8 +38,8 @@ public class MedianOfTwoSortedArrays {
     }
 
     public static void main(String[] args) {
-        int[] nums1 = new int[]{1, 3};
-        int[] nums2 = new int[]{2};
+        int[] nums1 = new int[]{1, 2};
+        int[] nums2 = new int[]{3, 4};
         System.out.println(findMedianSortedArrays(nums1, nums2));
     }
 }
