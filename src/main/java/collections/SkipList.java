cc8e7a777a7e07bc3dcb47935c3c9458d9a43091
wencaiwulue <895703375@qq.com>
2019-09-25 22:11:49 +0800

    'test'

---
+++ src/main/java/collections/SkipList.java
@@ -0,0 +1,9 @@
+package collections;
+
+/**
+ * actually it's a linked list, but it's ordered, so if we want to find some element,
+ * we want to use ordered feature, so we record more pointer
+ */
+public class SkipList {
+
+}
