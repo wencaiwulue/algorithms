9963f62b5f3b21ec399bb44dd60ad2cee6f2d4d6
wencaiwulue <895703375@qq.com>
2019-09-26 18:07:14 +0800

    test

--- src/main/java/algorithm/leecode/SymmetricTree.java
+++ src/main/java/algorithm/leecode/SymmetricTree.java
@@ -1,9 +1,8 @@
 package algorithm.leecode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * @author fengcaiwen
@@ -12,29 +11,115 @@ import java.util.Objects;
 public class SymmetricTree {
 
     public static void main(String[] args) {
-        System.out.println(match(Arrays.asList(4, null, null, 4)));
+        TreeNode node = new TreeNode(1);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(2);
+        node.left.left = null;
+        node.left.right = new TreeNode(3);
+        node.right.left = null;
+        node.right.right = new TreeNode(3);
+        System.out.println(isSymmetric(node));
     }
 
-    public boolean isSymmetric(TreeNode root) {
-        return false;
+    /*
+    *
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+*
+*
+*
+*   1
+   / \
+  2   2
+   \   \
+   3    3
+    *
+    * */
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+//        return test(Collections.singletonList(root));
+        // todo should work
+        return isTheSame(preorderReverse(root), suforderReverse(root));
     }
 
-    public boolean test(List<TreeNode> nodes) {
+    public static List<Integer> preorderReverse(TreeNode node) {
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(node);
+        List<Integer> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            TreeNode pop = stack.pop();
+            result.add(pop.val);
+            if (pop.left != null) {
+                stack.push(pop.left);
+            } else {
+//                result.add(null);
+            }
+            if (pop.right != null) {
+                stack.push(pop.right);
+            } else {
+//                result.add(null);
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    public static List<Integer> suforderReverse(TreeNode node) {
+        Queue<TreeNode> stack = new LinkedBlockingQueue<>();
+        stack.add(node);
+        List<Integer> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            TreeNode pop = stack.poll();
+            if (pop.left != null) {
+                stack.add(pop.left);
+            } else {
+//                result.add(null);
+            }
+            if (pop.right != null) {
+                stack.add(pop.right);
+            } else {
+//                result.add(null);
+            }
+            result.add(pop.val);
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    public static boolean isTheSame(List<Integer> list1, List<Integer> list2) {
+        if (list1 == null && list2 == null) return true;
+        if (list1 == null || list2 == null) return false;
+        if (list1.size() != list2.size()) return false;
+        boolean b = true;
+        for (int i = 0; i < list1.size(); i++) {
+            b &= Objects.equals(list1.get(i), list2.get(i));
+        }
+        return b;
+    }
+
+    public static boolean test(List<TreeNode> nodes) {
+        if (nodes.isEmpty()) return true;
         List<TreeNode> list = new ArrayList<>(nodes.size() * 2);
         List<Integer> ints = new ArrayList<>(nodes.size() * 2);
         for (TreeNode node : nodes) {
             if (node.left != null) {
                 list.add(node.left);
                 ints.add(node.left.val);
+            } else {
+                ints.add(null);
             }
             if (node.right != null) {
                 list.add(node.right);
                 ints.add(node.right.val);
+            } else {
+                ints.add(null);
             }
         }
-
-        match(ints);
-        return test(list);
+        System.out.println(ints);
+        return test(list) && match(ints);
     }
 
     private static boolean match(List<Integer> list) {
