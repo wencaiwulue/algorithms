37b0b8f7d900979a17219dfd62c311fc63d9b758
wencaiwulue <895703375@qq.com>
2019-09-29 18:06:48 +0800

    test

--- src/main/java/algorithm/leecode/MedianOfTwoSortedArrays.java
+++ src/main/java/algorithm/leecode/MedianOfTwoSortedArrays.java
@@ -1,11 +1,23 @@
-package algorithm;
+package algorithm.leecode;
 
 /**
  * @author fengcaiwen
  * @since 5/28/2019
  */
 public class MedianOfTwoSortedArrays {
-    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 2};
+        int[] nums2 = new int[]{3, 4};
+        long start = System.nanoTime();
+        double v = findMedianSortedArrays(nums1, nums2);
+        long end = System.nanoTime();
+        System.out.printf("%s ns\n", end - start);
+
+        System.out.println(v);
+    }
+
+    private static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
         int[] ints = new int[n + m];
@@ -70,16 +82,5 @@ public class MedianOfTwoSortedArrays {
         }
 
         return result;
-    }
-
-    public static void main(String[] args) {
-        int[] nums1 = new int[]{1, 2};
-        int[] nums2 = new int[]{3, 4};
-        long start = System.nanoTime();
-        double v = findMedianSortedArrays(nums1, nums2);
-        long end = System.nanoTime();
-        System.out.printf("%s ns\n", end - start);
-
-        System.out.println(v);
     }
 }
